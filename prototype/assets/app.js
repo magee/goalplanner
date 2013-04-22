61ea4992a66bc93394e728a87c125365efb7e811
Magee Mooney <mageemooney@gmail.com>
2013-04-22 10:40:15 -0700

    adds prototype header and menus

---
+++ prototype/assets/app.js
@@ -0,0 +1,17 @@
+$(function() {
+  $( ".datepicker" ).datepicker();
+});
+$(function() {
+  $( "#menu" ).menu();
+});
+$(function() {
+  // var icons = {
+  //   header: "ui-icon-circle-arrow-e",
+  //   activeHeader: "ui-icon-circle-arrow-s"
+  // };
+  $( "#accordion" ).accordion({
+    heightStyle: "content",
+    collapsible: true,
+    icons: null
+  });
+});
