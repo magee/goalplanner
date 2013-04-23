9401fe14367cb222dccde6bd769739fa6f710cba
Magee Mooney <mageemooney@gmail.com>
2013-04-22 21:04:56 -0700

    revises headers to tighten up screen layout

--- assets/app.js
+++ assets/app.js
@@ -1,17 +1,11 @@
-$(function() {
-  $( ".datepicker" ).datepicker();
-});
-$(function() {
-  $( "#menu" ).menu();
-});
-$(function() {
-  // var icons = {
-  //   header: "ui-icon-circle-arrow-e",
-  //   activeHeader: "ui-icon-circle-arrow-s"
-  // };
-  $( "#accordion" ).accordion({
-    heightStyle: "content",
-    collapsible: true,
-    icons: null
+$(document).ready(function () {
+  $("#add_new").on('click', function(){
+    $("#page_button").html("<div id='cancel' class='btn btn-primary btn-toggle-slide float-right'>cancel
+      <i class='icon slide-down'></i></div>");
   });
+
+  $("#cancel").on('click', function(){
+    $("#page_button").html("<div id='add_new' class='btn btn-primary btn-toggle-slide float-right'>add new goal<i class='icon slide-down'></i></div>");
+  });
+
 });
