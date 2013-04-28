de395ca29e648d46643e5d38dd249b1a30871bc7
Magee Mooney <mageemooney@gmail.com>
2013-04-28 16:00:32 -0700

    updates users from fixture, creates associations and modifies db

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -2,4 +2,5 @@ class Startup < ActiveRecord::Base
   attr_accessible :founder, :founder_email, :name
   has_many :milestones, :dependent => :destroy
   has_many :people
+  has_many :users
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130428213916) do
+ActiveRecord::Schema.define(:version => 20130428225649) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -107,6 +107,7 @@ ActiveRecord::Schema.define(:version => 20130428213916) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.integer  "startup_id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
