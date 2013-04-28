ffaf34f7aaebd1f82ed8b942c3e4ea6ee2b7f15b
Magee Mooney <mageemooney@gmail.com>
2013-04-28 12:10:22 -0700

    adds gmail config for test emailing

---
+++ config/environments/$RAILS_ENV.rb
@@ -0,0 +1,8 @@
+config.action_mailer.delivery_method = :smtp
+config.action_mailer.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :user_name            => 'mageemooney@gmail.com',
+  :password             => 'ntpDTakogyN0',
+  :authentication       => 'plain',
+  :enable_starttls_auto => true  }
