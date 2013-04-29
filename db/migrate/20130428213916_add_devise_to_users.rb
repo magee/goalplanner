60dc163f71e078ec89d0a8f1068d9c99c52f273f
Magee Mooney <mageemooney@gmail.com>
2013-04-28 17:08:32 -0700

    reverts migration to add an admin model in favor of an is_admin User attribute

--- db/migrate/20130428213916_add_devise_to_users.rb
+++ db/migrate/20130428213916_add_devise_to_users.rb
@@ -48,6 +48,18 @@ class AddDeviseToUsers < ActiveRecord::Migration
   def self.down
     # By default, we don't want to make any assumption about how to roll back a migration when your
     # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
+   change_table(:users) do |t|
+      remove_column :users, :encrypted_password
+      remove_column :users, :reset_password_token
+      remove_column :users, :reset_password_sent_at
+      remove_column :users, :remember_created_at
+      remove_column :users, :sign_in_count
+      remove_column :users, :current_sign_in_at
+      remove_column :users, :last_sign_in_at
+      remove_column :users, :current_sign_in_ip
+      remove_column :users, :last_sign_in_ip
+
+      remove_index :users, :reset_password_token
+    end
   end
 end
