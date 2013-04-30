9fc00a3a1f4e31baa69349ed1437c613670f20de
Magee Mooney <mageemooney@gmail.com>
2013-04-29 22:54:20 -0700

    adds tmp to .gitignore, goal_id to task model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130428225649) do
+ActiveRecord::Schema.define(:version => 20130430023811) do
 
   create_table "goals", :force => true do |t|
     t.string   "title"
@@ -70,6 +70,7 @@ ActiveRecord::Schema.define(:version => 20130428225649) do
     t.boolean  "is_template"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+    t.integer  "goal_id"
   end
 
   create_table "users", :force => true do |t|
